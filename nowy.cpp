aa99cd7ad8b22f77420889391b9518c344bec4d6
poewer <poewer1@gmail.com>
2022-01-07 22:57:49 +0100

    Second commit

--- nowy.cpp
+++ nowy.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-	string a = "Hello World";
-	std::cout<<a<<std::endl;
-
+	string a = "Hello World"; //define variable type string 
+	std::cout<<a<<std::endl; //hello world
+	
 	return 0;
 }
