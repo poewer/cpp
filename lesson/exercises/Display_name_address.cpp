3a2b4682cdf04d6638223752584449da242bbccf
Michał Białek <poewer1@gmail.com>
2022-01-08 13:39:36 +0100

    Description what the program doing

--- lesson/exercises/Display_name_address.cpp
+++ lesson/exercises/Display_name_address.cpp
@@ -3,10 +3,10 @@
 
 
 int main(){
-	std::string your_name = "Marek";
-	char address[100] ="New_addres 38A";
-	std::cout<<"Your name: "<< your_name <<"\n";
-	std::cout<<"Your address: "<<address<<std::endl;
+	std::string your_name = "Marek";			//Declare variable your_name as type string 
+	char address[100] ="New_addres 38A";			//Declare variable your_name as type char 
+	std::cout<<"Your name: "<< your_name <<"\n";		//Display name
+	std::cout<<"Your address: "<<address<<std::endl;	//Display address 
 
 	return 0;
 }
