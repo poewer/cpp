3fc52cef9add15fbdd9ea644918de5413ab67e99
Michał Białek <poewer1@gmail.com>
2022-01-08 13:12:05 +0100

    Description variables in cpp

---
+++ lesson/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+	char a[3]="Cat";	//Character types
+	int b = 5;		//Integer types (signed)
+	unsigned int b_1 = 6;	//Integer types (unsigned)
+	float c = 1.0f;		//Floating-point types
+	double d = 2.0;		//Floating-point types
+	bool e = true;		//Boolean type
+
+
+	return 0;
+}
