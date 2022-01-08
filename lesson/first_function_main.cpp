203e1133ad331f69d49bda4d2553670bf4c17797
Michał Białek <poewer1@gmail.com>
2022-01-08 13:03:15 +0100

    Added comments for 'endl'

--- lesson/first_function_main.cpp
+++ lesson/first_function_main.cpp
@@ -4,7 +4,8 @@ using namespace std;		//Header of function
 
 int main(){			//Declare of function  
 	cout<<"Hello World";	//Display Hello World
-	cout<<endl;		//new line 	
+	cout<<endl;		//new line. We can also used "\n" it works the same.  	
 	
 	return 0;		//return value of  function 
 }
+
