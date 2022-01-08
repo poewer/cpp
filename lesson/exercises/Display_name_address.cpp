eb549ad45ee5c4ff805e69a093935809e1d7cbf8
Michał Białek <poewer1@gmail.com>
2022-01-08 13:31:19 +0100

    Finished exercises/Display_name_address.cpp

---
+++ lesson/exercises/Display_name_address.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+	std::string your_name = "Marek";
+	char address[100] ="New_addres 38A";
+	std::cout<<"Your name: "<< your_name <<"\n";
+	std::cout<<"Your address: "<<address<<std::endl;
+
+	return 0;
+}
